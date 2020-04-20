15a39cadf2b4e48abff0bd12bdbb28886647d9ac
Annie Davis <anniecdavis27@gmail.com>
2020-04-20 11:20:49 -0400

    Updates

---
+++ precache-manifest.b4b17090d27047eddf9cbdafb129ff66.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f55ca8624f64cf92d3bf23b6c26c73a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d5a11fc21830586925a",
+    "url": "/static/css/main.719efd0e.chunk.css"
+  },
+  {
+    "revision": "abf85cc1d2c7ddbc3dd9",
+    "url": "/static/js/2.38d42657.chunk.js"
+  },
+  {
+    "revision": "78aa0676509153305bde072ed1e9cb1c",
+    "url": "/static/js/2.38d42657.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d5a11fc21830586925a",
+    "url": "/static/js/main.7307e162.chunk.js"
+  },
+  {
+    "revision": "228fa098814da32aa5c2",
+    "url": "/static/js/runtime-main.e1daef6b.js"
+  }
+]);
